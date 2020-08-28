c54d703f7d220dd312df7bca84214cf2752d6fc5
zeeshan junaid <zeeshanjunaid2222@gmail.com>
2020-08-29 00:41:52 +0500

    fixed mobile contact us issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ContactWidget from "./components/contactWidget";
@@ -6,6 +6,10 @@ import Help from "./components/Help";
 import Category from "./components/Category";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
   return (
     <Router>
       <div className="App">
